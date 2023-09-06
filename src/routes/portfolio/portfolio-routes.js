04bd34eed6cb1fe821cb5b7858effe0379d2d4fb
Malzagic <przemek2142@gmail.com>
2023-09-06 20:59:01 +0200

    comment options in get request

--- src/routes/portfolio/portfolio-routes.js
+++ src/routes/portfolio/portfolio-routes.js
@@ -9,16 +9,16 @@ const portfolioRoutes = router.get("/", async (req, res) => {
     try {
         const url = "https://api.github.com/search/repositories?q=user:malzagic&sort=starred"
         const token = process.env.REACT_GITHUB_TOKEN;
-        const options = {
-            method: "GET",
-            headers: {
-                // 'Access-Control-Allow-Origin': '*',
-                "Accept": "application/vnd.github+json",
-                "Authorization": `Bearer ${token}`,
-            }
-        }
+        // const options = {
+        //     method: "GET",
+        //     headers: {
+        //         // 'Access-Control-Allow-Origin': '*',
+        //         "Accept": "application/vnd.github+json",
+        //         "Authorization": `Bearer ${token}`,
+        //     }
+        // }
 
-        const response = await axios.get(url, options);
+        const response = await axios.get(url);
 
         const data = response.data.items.filter(item => {
             return item.stargazers_count > 0;
