5ef892b11cfb5274d3d184e39833ee0df59a01a5
Malzagic <przemek2142@gmail.com>
2023-09-06 20:54:05 +0200

    refresh

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://portfolio-red-iota-82.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // You can also set more headers as needed, such as methods and headers
     // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
