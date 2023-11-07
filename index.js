7fd4aee5407813bb80acdd2d7a7481d6b97ff877
Malzagic <przemek2142@gmail.com>
2023-11-07 16:25:04 +0100

    fix route

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 // ROUTES
-express.static(path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
