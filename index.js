82259284eed288185790307ea8effa2670883722
Malzagic <malzagic@MacBook-Pro-Malzagic.local>
2023-12-02 06:54:08 +0100

    big update to handle site on my server only with my domain

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
 import express from "express";
 import * as path from "path";
-import { fileURLToPath } from "url";
-import cors from "cors";
 import bodyParser from "body-parser";
-import { homeRoute } from "./src/routes/serverSite/home-route.js";
-import { portfolioRoutes } from "./src/routes/portfolio/portfolio-routes.js";
-import { contactRoutes } from "./src/routes/portfolio/contact-routes.js";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { portfolioRoutes } from "./routes/portfolio/portfolio-routes.js";
+import { contactRoutes } from "./routes/portfolio/contact-routes.js";
 
 // STARTING POINT
 const app = express();
 const port = process.env.PORT || 8000;
 
 // ROUTES
-app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
-app.use("/", homeRoute);
+app.use(express.static(path.join("public")));
 
 app.use("/api/portfolio", portfolioRoutes);
 
 app.use("/api/contact", contactRoutes);
 
 app.use((req, res, next) => {
-  res.sendFile(path.resolve(__dirname, "views", "index.ejs"));
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
 });
 
 app.listen(port, () => {
